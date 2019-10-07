1a8b9a154577fbef2abd02b74355ae6845430b49
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2019-10-07 21:09:03 +0900

    404も関数として同じように扱うようにした

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,7 @@
 $routes = [];
 
 $routes['/'] = function() {
+    ob_start();
     include __DIR__ . '/../app/view/index.phtml';
     return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
 };
--- public/index.php
+++ public/index.php
@@ -6,15 +6,14 @@ $routes = require __DIR__. '/../app/routes.php';
 
 $request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if (isset($routes[$request_uri])) {
-    $f = $routes[$request_uri];
-    [$status, $headers, $body] = $f();
-    http_response_code($status);
-    foreach ($headers as $name => $h) {
-        header("{$name} {$h}");
-    }
-    echo $body;
-} else {
-    http_response_code(404);
-    echo "<h1> 404 not found</h1>";
+$not_found = function () {
+    return [404, ['Content-Type' => 'text/html'], "<h1> 404 not found</h1>"];
+};
+
+$f = $routes[$request_uri] ?? $not_found;
+[$status, $headers, $body] = $f();
+http_response_code($status);
+foreach ($headers as $name => $h) {
+    header("{$name} {$h}");
 }
+echo $body;
