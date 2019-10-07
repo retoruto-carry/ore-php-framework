01502eeb020d369f89e738d9060161e03db8046a
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2019-10-07 21:57:13 +0900

    テンプレート化した

--- app/routes.php
+++ app/routes.php
@@ -1,11 +1,13 @@
 <?php
 
 $routes = [];
+$template = new \Oira\TemplateFactory(__DIR__ . '/view/');
 
-$routes['/'] = function() {
-    ob_start();
-    include __DIR__ . '/../app/view/index.phtml';
-    return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
+$routes['/'] = function() use ($template) {
+    return [200, ['Content-Type' => 'text/html'], $template->create('index', [
+        'title' => 'れとるときゃりーのホームページのindex',
+        'name' => 'れとるときゃりー'
+    ])];
 };
 
 $routes['/phpinfo.php'] = function() {
