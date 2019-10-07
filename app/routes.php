96b327784b46c34e16112444c55c05f518bdf4c2
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2019-10-07 20:16:30 +0900

    src/index.php と app/routes.php に分割した

--- app/routes.php
+++ app/routes.php
@@ -1,1 +1,17 @@
 <?php
+
+$routes = [];
+$routes['/'] = function() {
+    echo "<!DOCTYPE html>\n";
+    echo "<title>test</title>\n";
+    echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')). "です</p>\n";
+    echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
+    exit;
+};
+
+$routes['/phpinfo.php'] = function() {
+    phpinfo();
+    exit;
+};
+
+return  $routes;
--- public/index.php
+++ public/index.php
@@ -2,22 +2,7 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// var_dump($_SERVER['REQUEST_URI']);exit;
-
-$routes = [];
-$routes['/'] = function() {
-    echo "<!DOCTYPE html>\n";
-    echo "<title>test</title>\n";
-    echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')). "です</p>\n";
-    echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
-    exit;
-};
-
-$routes['/phpinfo.php'] = function() {
-    phpinfo();
-    exit;
-};
-
+$routes = require __DIR__. '/../app/routes.php';
 
 if (isset($routes[$_SERVER['REQUEST_URI']])) {
     $f = $routes[$_SERVER['REQUEST_URI']];
