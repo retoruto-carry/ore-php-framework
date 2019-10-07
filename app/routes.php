2c204229c89c5f1b6bcfb89ecd6cd99f2fd799bd
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2019-10-07 20:47:57 +0900

    headerなどを返すようにした

--- app/routes.php
+++ app/routes.php
@@ -4,10 +4,14 @@ $routes = [];
 
 $routes['/'] = function() {
     include __DIR__ . '/../app/view/index.phtml';
+    return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
 };
 
 $routes['/phpinfo.php'] = function() {
+    ob_start();
     phpinfo();
+
+    return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
 };
 
 return  $routes;
--- public/index.php
+++ public/index.php
@@ -8,7 +8,12 @@ $request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if (isset($routes[$request_uri])) {
     $f = $routes[$request_uri];
-    $f();
+    [$status, $headers, $body] = $f();
+    http_response_code($status);
+    foreach ($headers as $name => $h) {
+        header("{$name} {$h}");
+    }
+    echo $body;
 } else {
     http_response_code(404);
     echo "<h1> 404 not found</h1>";
